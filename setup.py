d325cf4f3b380386263b8dbda117ffd1d9a0c589
Matthew K <thefrogmaster@gmail.com>
2016-05-25 14:30:51 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'python-firebase',
     packages = find_packages(),
-    version = '0.1.2',
+    version = '0.1.3',
     description = "Simple wrapper around Firebase's REST API",
     author = 'Michael Huynh',
     author_email = 'mike@mikexstudios.com',
